fafa69cb67ccfb6bb7a6d7c4c7078fbd05330b32
Ronnie Rios <ronniesmail14@gmail.com>
2021-09-06 12:41:17 -0500

    setup componets

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,8 @@ import { StreamChat } from 'stream-chat';
 import { Chat } from 'stream-chat-react';
 import Cookies from 'universal-cookie';
 
+
+import { ChannelContainer, ChannelListContainer } from './components';
 const apiKey = 'fyf6zkcvpeez';
 
 const client = StreamChat.getInstance(apiKey);
