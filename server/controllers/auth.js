e842730dddd2c4125db406931152cfcd65aa2def
Ronnie Rios <ronniesmail14@gmail.com>
2021-09-25 09:59:57 -0500

    created login function for controler

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -7,14 +7,6 @@ const api_key = process.env.STREAM_API_KEY;
 const api_secret = process.env.STREAM_API_SECRET;
 const app_id= process.env.STREAM_APP_ID;
 
-const login= (req, res) => {
-    try {
-
-    } catch(error) {
-        console.log(error);
-        res.status(500).json({ message: error });
-    }
-};
 const signup = async (req, res) => {
     try {
         const {fullName, username, password, phoneNumber } = req.body;
@@ -28,6 +20,34 @@ const signup = async (req, res) => {
         const token = serverClient.createUserToken(userId);
 
         res.status(200).json({ token, fullName, username, userId, hashedPassword, phoneNumber })
+
+    } catch(error) {
+        console.log(error);
+        res.status(500).json({ message: error });
+    }
+};
+
+
+const login = async(req, res) => {
+    try {
+        const { username, password } = req.body;
+
+        const serverClient = connect(api_key, api_secret, app_id);
+        const client = StreamChat.getInstance(api_key, api_secret);
+
+        const { users } = await client.queryUsers({ name: username });
+
+        if(!users.length) return res.status(400).json({ message: 'user not found'});
+
+        const success = await bcrypt.compare(password, users[0].hashedPassword);
+
+        const token = serverClient.createUserToken(users[0].id);
+
+        if (success) {
+            res.status(200).json({ token, fullname: users[0].fullname, username, userId: users[0].id });
+        } else {
+            res.status(500).json({ message: error });
+        }
 
     } catch(error) {
         console.log(error);
